ee232d21c9438ed328c0e7c6f7a99eb4541b5f0b
Abner Willclefy <abnerwill@hotmail.com>
2021-06-11 00:15:03 -0300

    Homepage finished

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,16 +4,24 @@ export default function Home() {
     return (
         <div className={styles.homepage}>
             <header className={styles.headerContainer}>
-                <span className={styles.logo}>
+                <h1 className={styles.logo}>
                     Healthy Foods
-                </span>
+                </h1>
                 <nav className={styles.menu}>
-                    <a>HEALTHY RECIPES</a>
-                    <a>BLOG</a>
-                    <a>JOIN</a>
-                    <a>REGISTER</a>
+                    <span>HEALTHY RECIPES</span>
+                    <span>BLOG</span>
+                    <span>JOIN</span>
+                    <span>REGISTER</span>
                 </nav>
             </header>
+
+            <div className={styles.innerContainer}>
+                <h1>Ready for<br/> Trying a new recipe?</h1>
+                <div className={styles.sendText}>
+                    <input type="text" placeholder="Search healthy recipes"/>
+                    <button><img src="assets/search.png" alt="search icon" /></button>
+                </div>
+            </div>
         </div>
     )
 }
